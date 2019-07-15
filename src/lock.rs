d31907a9e16efaac8439c82f7bbe8d31ef948075
Dany Laporte <dany_laporte@hotmail.com>
2019-07-15 09:27:04 -0400

    implement default and new for lock

--- src/lock.rs
+++ src/lock.rs
@@ -1,21 +1,23 @@
 use crate::{ConnOrFactory, LoadFromConn, ReadFut, ReadOptFut, SetTag, WriteFut, WriteOptFut};
 use futures_locks::RwLock;
 
-pub struct Lock<T> {
-    lock: RwLock<Option<T>>,
-}
+pub struct Lock<T>(RwLock<Option<T>>);
 
 impl<T> Lock<T> {
+    pub fn new(value: Option<T>) -> Self {
+        Self(RwLock::new(value))
+    }
+
     pub fn read<C>(&self, conn: C) -> ReadFut<T>
     where
         C: Into<ConnOrFactory>,
         T: LoadFromConn,
     {
-        ReadFut::load(conn.into(), self.lock.clone())
+        ReadFut::load(conn.into(), self.0.clone())
     }
 
     pub fn read_opt(&self) -> ReadOptFut<T> {
-        ReadOptFut::load(self.lock.read())
+        ReadOptFut::load(self.0.read())
     }
 
     pub fn write<C>(&self, conn: C) -> WriteFut<T>
@@ -23,13 +25,19 @@ impl<T> Lock<T> {
         C: Into<ConnOrFactory>,
         T: LoadFromConn + SetTag,
     {
-        WriteFut::load(conn.into(), self.lock.write())
+        WriteFut::load(conn.into(), self.0.write())
     }
 
     pub fn write_opt(&self) -> WriteOptFut<T>
     where
         T: SetTag,
     {
-        WriteOptFut::load(self.lock.write())
+        WriteOptFut::load(self.0.write())
+    }
+}
+
+impl<T> Default for Lock<T> {
+    fn default() -> Self {
+        Self(RwLock::new(None))
     }
 }
